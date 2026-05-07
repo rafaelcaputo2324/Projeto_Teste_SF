8fe100ded0df62f0f3c1ce3de45b0d373deb77dc
Nataliacnobre <nataliacnobre@gmail.com>
2026-05-07 20:42:44 -0300

    testes da api

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('./database/database.sqlite');
+const db = new sqlite3.Database('database.sqlite');
 
 db.serialize(() => {
   db.run(`
