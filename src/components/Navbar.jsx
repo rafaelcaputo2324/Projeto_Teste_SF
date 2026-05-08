e6e72391c3e1a3f78fc58b4872d2983dd802ed51
mirelle0506 <mirellerd2006@gmail.com>
2026-05-08 11:08:23 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,17 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="navbar">
+
       <h2>LoginPlus</h2>
 
       <div>
         <Link to="/dashboard">Home</Link>
-        <Link to="/produtos">Produtos</Link>
+
+        <Link to="/produtos">
+          Produtos
+        </Link>
       </div>
+
     </nav>
   );
 }
