c3c10badbb24c7f603848bd68b2f45df94d0e43e
joaovithor2002-png <joaovithor2002@gmail.com>
2026-05-08 01:31:06 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Products from "./pages/Products";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/produtos" element={<Products />} />
       </Routes>
     </BrowserRouter>
   );
