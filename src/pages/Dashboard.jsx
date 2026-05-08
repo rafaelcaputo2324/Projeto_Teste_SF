2870ca8e9f81cb7b92b9326d647383b7219ebb54
mirelle0506 <mirellerd2006@gmail.com>
2026-05-08 11:07:18 -0300

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,9 +6,13 @@ function Dashboard() {
       <Navbar />
 
       <div className="dashboard">
-        <h1>Bem-vindo ao LoginPlus</h1>
+
+        <h1>
+          Bem-vindo ao LoginPlus
+        </h1>
 
         <div className="cards">
+
           <div className="card">
             <h2>Usuários</h2>
             <p>15 cadastrados</p>
@@ -18,7 +22,9 @@ function Dashboard() {
             <h2>Produtos</h2>
             <p>32 cadastrados</p>
           </div>
+
         </div>
+
       </div>
     </>
   );
